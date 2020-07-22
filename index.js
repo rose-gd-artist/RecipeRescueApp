a90e9f13aeb070dde7d19af3c05d59d29f110553
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-07-22 17:34:39 -0400

    Added quantity fields next to each grocery list recipe item.

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ $(document).ready(function(){
 
     submit.addEventListener("click", getRecipe);
 
-    function getRecipe(){
-
+    function getRecipe() {
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -19,61 +18,59 @@ $(document).ready(function(){
                 "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
                 "x-rapidapi-key": recipePuppyAPI
             }
-        } /* end of settings */
+        }
         
         $.ajax(settings).done(function (response) {
             console.log(response);
             displayRecipes(response);
-        }); /* end of ajax fx */
-
-        function displayRecipes(searchedIngredients){
-
-            let answer = searchedIngredients;
-            let newAnswer = JSON.parse(answer);
-
-            resultsBox.innerHTML = ""; /* clear previous food result boxes */
-
-            for(let i = 0; i < newAnswer.results.length; i++){
-                let recipeListing = document.createElement("div");
-                resultsBox.append(recipeListing);
-                recipeListing.classList.add("food-block");
-                recipeListing.innerHTML = "<div class='food-image'></div>";
-                /* insert food background image ('background-size' property is set to 'cover' so each food image will be the same size) */
-                $(".food-image").eq($(this).index()).css({
-                    "background-image": "url('" + newAnswer.results[i].thumbnail + "')"
-                });
-                recipeListing.innerHTML += "<div class='food-title-and-link'>" + newAnswer.results[i].title + "<br>" + "<a href='" + newAnswer.results[i].href + "' target='_blank'>Link to Recipe</a> <div class='add-to-grocery-list'>Add to Grocery List</div></div>";
-                
-                /* adding ingredients to grocery list */
-                let currentGroceryItem = document.getElementsByClassName("add-to-grocery-list")[i];
-
-                currentGroceryItem.addEventListener("click", function () {
-
-                    /* split ingredients list returned from API and create bullets */
-                    let splitIngredientsList = newAnswer.results[i].ingredients.split(",");
-                    let bulletedList = "";
-
-                    for (let i = 0; i < splitIngredientsList.length; i++) {
-                        bulletedList += "<span>&#8226; </span>" + splitIngredientsList[i] + "<br>";
-                    }
-
-                    groceryListContainer.innerHTML += "<div class='grocery-list-entry'><strong>" + "<a href='" + newAnswer.results[i].href + "' target='_blank'>" + newAnswer.results[i].title + "</a>" + ":</strong><br>" + bulletedList + "<br><span class='remove-item'>remove item</span><br></div>";
-
-                    /* removing items from a grocery list */
-                    let removeItem = document.getElementsByClassName("remove-item");
-                    let groceryListEntry = document.getElementsByClassName("grocery-list-entry");
-
-                    for (let i = 0; i < removeItem.length; i++) {
-                        removeItem[i].addEventListener("click", function () {
-                            $('.grocery-list-entry').eq(i).fadeOut(400);
-                        });
-                    }
-                });
-
-            } /* end of for loop */
-
-        } /* end of displayRecipe fx */
+        }); 
     } /* end of getRecipe fx */
+
+    function displayRecipes(searchedIngredients){
+        let answer = searchedIngredients;
+        let newAnswer = JSON.parse(answer);
+
+        resultsBox.innerHTML = ""; /* clear previous food result boxes */
+
+        for(let i = 0; i < newAnswer.results.length; i++){
+            let recipeListing = document.createElement("div");
+            recipeListing.classList.add("food-block");
+            recipeListing.innerHTML = "<div class='food-image'></div>";
+            resultsBox.append(recipeListing);
+            /* insert food background image ('background-size' property is set to 'cover' so each food image will be the same size) */
+            $(".food-image").eq($(this).index()).css({
+                "background-image": "url('" + newAnswer.results[i].thumbnail + "')"
+            });
+
+            /* insert food title, recipe link, and add to grocery list button */
+            recipeListing.innerHTML += "<div class='food-title-and-link'>" + newAnswer.results[i].title + "<br>" + "<a href='" + newAnswer.results[i].href + "' target='_blank'>Link to Recipe</a> <div class='add-to-grocery-list'>Add to Grocery List</div></div>";
+                
+            /* adding ingredients to grocery list */
+            let currentGroceryItem = document.getElementsByClassName("add-to-grocery-list")[i];
+
+            currentGroceryItem.addEventListener("click", function () {
+                /* split ingredients list returned from API and create bullets */
+                let splitIngredientsList = newAnswer.results[i].ingredients.split(",");
+                let bulletedList = "";
+
+                for (let i = 0; i < splitIngredientsList.length; i++) {
+                    bulletedList += "<span>&#8226; </span>" + splitIngredientsList[i] + "<input class='quantity-field' placeholder='QTY' type='text'><br>";
+                }
+
+                groceryListContainer.innerHTML += "<div class='grocery-list-entry'><strong>" + "<a href='" + newAnswer.results[i].href + "' target='_blank'>" + newAnswer.results[i].title + "</a>" + ":</strong><br>" + bulletedList + "<br><span class='remove-item'>remove item</span></div>";
+
+                /* removing items from a grocery list */
+                let removeItem = document.getElementsByClassName("remove-item");
+                let groceryListEntry = document.getElementsByClassName("grocery-list-entry");
+
+                for (let i = 0; i < removeItem.length; i++) {
+                    removeItem[i].addEventListener("click", function () {
+                        $('.grocery-list-entry').eq(i).fadeOut(400);
+                    });
+                } 
+            }); /* end click event */
+        } /* end of for loop */
+    } /* end of displayRecipe fx */
 
     /* download/print PDF of grocery list (print settings specified in CSS print media query) */
     let exportPDF = document.getElementById("exportPDF");
